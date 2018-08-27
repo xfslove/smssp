a5ffd17cee5078f6926353f56db186050ee6e933
hanwen.huang <xfslove@gmail.com>
2018-08-27 17:38:04 +0800

    add boot application

---
+++ src/main/java/com/github/xfslove/Application.java
@@ -0,0 +1,16 @@
+package com.github.xfslove;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * @author hanwen
+ * created at 2018/8/27
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder().build().run(args);
+  }
+}
